29ed7b48a9923dff9e8b5273d13faeb56f3d3b2d
mfro <mfro@pm.me>
2025-07-28 14:29:15 -0500

    kms

--- RecallConsole/Program.fs
+++ RecallConsole/Program.fs
@@ -2,24 +2,15 @@
 open System.IO
 open RecallLib
 
-let editorNote () =
-    printfn "Editor notes are not implemented. Launch with an argument."
-    failwith "NotImplemented"
-    // I think for now I will just have this open edit.exe or nano in linux
-    // getting env vars in windows is probably a pain
-
-let insertArgsNote args =
-    let note: string = Array.get args 0
-    let tags: string = Array.get args 1
-    let date: DateTime = System.DateTime.Now
-    let entry  = [| note, tags, date.ToString() |]
-    File.AppendAllText("testPile.txt", $"note: {note}),({tags}),({date})\n")
-    File.AppendAllLines("testPile.txt", entry)
-    0
-
 [<EntryPoint>]
 let main args =
+    if not(File.Exists("testCSV.csv")) then
+        File.WriteAllText("testCSV.csv", "NOTE, TAGS, DATE")
+        ()
     if Array.isEmpty(args) then
-        editorNote ()
+        RecallLib.Library.editorNote ()
+        1
     else
-        insertArgsNote args
+        let entry = RecallLib.Library.createEntry args
+        RecallLib.Library.insertEntry entry
+        0
--- RecallLib/Library.fs
+++ RecallLib/Library.fs
@@ -1,4 +1,25 @@
 ﻿namespace RecallLib
 
-    module Library =
-        open System.Text.Json
+module Library =
+    open System
+    open System.IO
+    open FSharp.Data
+
+    type Entry = CsvProvider<Schema="NOTE (string), TAGS, DATE (date)", HasHeaders=false>
+
+    let editorNote () =
+        printfn "Editor notes are not implemented. Launch with two arguments."
+        failwith "NotImplemented"
+    // I think for now I will just have this open edit.exe or nano in linux
+    // getting env vars in windows is probably a pain
+
+    let createEntry args =
+        let note: string = Array.get args 0
+        let tags: string = Array.get args 1
+        let date: DateTime = System.DateTime.Now
+        new Entry([ Entry.Row(note, tags, date) ])
+
+
+    let insertEntry (entry:Entry) =
+        let csv = Entry.Load("testCSV.csv").Append(entry.Rows) // csvprovider fails to load csv if no rows, create with headers fucks up csv.Save...
+        csv.Save("testCSV.csv")
