b1c0ed4582768cc67d621557c6962acc4d433960
mfroach <mfro@pm.me>
2025-07-27 20:07:47 -0500

    update

--- RecallConsole/Program.fs
+++ RecallConsole/Program.fs
@@ -1,19 +1,25 @@
 ﻿open System
+open System.IO
 open RecallLib
 
-let editorNote =
-    0 // open editor, ask for tags,
+let editorNote () =
+    printfn "Editor notes are not implemented. Launch with an argument."
+    failwith "NotImplemented"
+    // I think for now I will just have this open edit.exe or nano in linux
+    // getting env vars in windows is probably a pain
 
 let insertArgsNote args =
-    let note: string = args[0]
-    let tags = args[1]
+    let note: string = Array.get args 0
+    let tags: string = Array.get args 1
+    let date: DateTime = System.DateTime.Now
+    let entry  = [| note, tags, date.ToString() |]
+    File.AppendAllText("testPile.txt", $"note: {note}),({tags}),({date})\n")
+    File.AppendAllLines("testPile.txt", entry)
     0
 
 [<EntryPoint>]
 let main args =
-    if String.IsNullOrEmpty(args[0]) then
-        editorNote
+    if Array.isEmpty(args) then
+        editorNote ()
     else
         insertArgsNote args
-        
-    0
