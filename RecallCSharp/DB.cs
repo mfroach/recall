dc6181538fc0f28a18e4a2f08438ae82127ba784
mfro <mfro@pm.me>
2025-08-22 15:26:22 -0500

    update

--- RecallCSharp/DB.cs
+++ RecallCSharp/DB.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using Microsoft.Data.Sqlite;
 
 namespace RecallCSharp;
@@ -11,7 +12,7 @@ public class DB {
 
         var command = connection.CreateCommand();
         command.CommandText =
-        @"
+            @"
                 CREATE TABLE IF NOT EXISTS notes (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     note TEXT NOT NULL,
@@ -21,30 +22,56 @@ public class DB {
             ";
         command.ExecuteNonQuery();
     }
-    
-    public string RunQuery(SqliteCommand query) {
+
+    public List<Dictionary<string, object>> RunQuery(SqliteCommand query) {
         using var connection = new SqliteConnection(_connectionString);
         connection.Open();
         query.Connection = connection;
-        return query.ExecuteScalar().ToString();
+        using var result = query.ExecuteReader();
+    
+        var data = new List<Dictionary<string, object>>();
+        while (result.Read()) {
+            var row = new Dictionary<string, object>();
+            for (int i = 0; i < result.FieldCount; i++) {
+                row[result.GetName(i)] = result.GetValue(i);
+            }
+            data.Add(row);
+        }
+        return data;
     }
 
-    public SqliteCommand? QueryBuilder(string type, Entry queryData) {
+    public SqliteCommand QueryBuilder(string type, Entry entry) {
         var query = new SqliteCommand();
-        switch (type) {
-            case "insert":
-                query.CommandText = "INSERT INTO notes (note, tags, date) VALUES (@note, @tags, @date);";
-                query.Parameters.AddWithValue("@note", queryData.note);
-                query.Parameters.AddWithValue("@tags", string.Join(",", queryData.tags));
-                query.Parameters.AddWithValue("@date", queryData.date.ToString("o"));
-                return query;
-            case "search":
-                throw new NotImplementedException();
+        if (type == "insert") {
+            query.CommandText = "INSERT INTO notes (note, tags, date) VALUES (@note, @tags, @date);";
+            query.Parameters.AddWithValue("@note", entry.note);
+            query.Parameters.AddWithValue("@tags", string.Join("%", entry.tags));
+            query.Parameters.AddWithValue("@date", entry.date.ToString("o"));
         }
 
-        return null;
+        return query;
+    }
+
+    public SqliteCommand QueryBuilder(string type, string[] tags) {
+        var query = new SqliteCommand();
+        if (type == "search") {
+            query.CommandText = "SELECT * FROM notes WHERE tags LIKE @tags";
+            query.Parameters.AddWithValue("@tags", "%" + string.Join(",", tags) + "%");
+        }
+
+        return query;
+    }
+
+    public static Entry CreateEntry() {
+        Console.WriteLine("Enter note:");
+        var note = Console.ReadLine();
+        if (!string.IsNullOrEmpty(note)) {
+            Console.WriteLine("Enter tags, comma separated:");
+            string[] tags = Console.ReadLine()?.ToLowerInvariant().Split(",", StringSplitOptions.TrimEntries) ?? [];
+            var date = DateTime.Now;
+            return new Entry(note, tags, date);
+        } else throw new NullReferenceException("You didn't enter anything.");
     }
 
     public record Entry(string note, string[] tags, DateTime date);
-
 }
--- RecallCSharp/Program.cs
+++ RecallCSharp/Program.cs
@@ -1,21 +1,27 @@
-﻿using System;
-using RecallCSharp;
+﻿using System.Data.Common;
 
+namespace RecallCSharp;
 
 class Program {
     static void Main() {
         var db = new DB();
         db.InitializeDatabase();
-        var entry = CreateEntry();
-        db.RunQuery(db.QueryBuilder("insert", entry));
-    }
-
-    private static DB.Entry CreateEntry() {
-        Console.WriteLine("Enter note:");
-        var note = Console.ReadLine();
-        Console.WriteLine("Enter tags, comma separated:");
-        string[] tags = Console.ReadLine().Trim().ToLowerInvariant().Split(",");
-        var date = DateTime.Now;
-        return new DB.Entry(note, tags, date);
+        Console.WriteLine("Enter command (search, new):");
+        var op = Console.ReadLine();
+        switch (op) {
+            case "new":
+                var entry = DB.CreateEntry();
+                db.RunQuery(db.QueryBuilder("insert", entry));
+                break;
+            case "search":
+                Console.WriteLine("Enter tags to search, separated by commas");
+                var tags = Console.ReadLine()?.ToLowerInvariant().Split(",", StringSplitOptions.TrimEntries) ?? [];
+                var result = db.RunQuery(db.QueryBuilder("search", tags));
+                Console.WriteLine(result);
+                foreach (var i in result) {
+                    throw new NotImplementedException(); // Implement reading rows
+                }
+                break;
+        }
     }
 }
